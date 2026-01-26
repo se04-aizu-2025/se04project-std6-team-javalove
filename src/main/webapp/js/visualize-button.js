dfff0b59f49bb7473d9242f74a98b95a344a1e39
s1310172 <s1310172@u-aizu.ac.jp>
2026-01-27 03:10:16 +0900

    Delete src/main directory

--- src/main/webapp/js/visualize-button.js
+++
@@ -1,9 +0,0 @@
-const chk = document.getElementById('visualizeChk');
-const delayBox = document.getElementById('delayBox');
-
-function updateDelayVisibility() {
-  delayBox.style.display = chk.checked ? 'inline-flex' : 'none';
-}
-
-chk.addEventListener('change', updateDelayVisibility);
-updateDelayVisibility();
